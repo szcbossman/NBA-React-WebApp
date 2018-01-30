189defdf0843b323b25b70f1d78c0ad9063628e6
szcbossman <songzicheng3@gmail.com>
2018-01-29 19:47:05 -0800

    nba official logo from stats.nba.com

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import '../styles/App.css';
+import React, { Component } from 'react'
+import '../styles/App.css'
 import { TopNavBar } from './TopNavBar'
 
 class App extends Component {
