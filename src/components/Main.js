83eb07eed9587d4ab46c5450928893241831d709
szcbossman <songzicheng3@gmail.com>
2018-01-29 21:36:23 -0800

    profileInfo rendered into Profile component

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,19 +5,22 @@ import { Profile } from './Profile'
 
 export class Main extends React.Component {
   state = {
-    playerId: nba.findPlayer('Stephen Curry').playerId
+    playerId: nba.findPlayer('Stephen Curry').playerId,
+    playerInfo: {},
   }
 
   componentDidMount() {
     nba.stats.playerInfo({ PlayerID: this.state.playerId }).then((info) => {
-      console.log(info);
+      const playerInfo = Object.assign(info.commonPlayerInfo[0], info.playerHeadlineStats[0]);
+      console.log(playerInfo);
+      this.setState({ playerInfo: playerInfo });
     })
   }
 
   render () {
     return (
       <div className="main">
-        <Profile/>
+        <Profile playerInfo={this.state.playerInfo}/>
         <ShotChart playerId={this.state.playerId}/>
       </div>
     );
--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,57 @@
-import React from 'react'
+import React from 'react';
+import { PROFILE_PIC_URL_PREFIX, TEAM_PIC_URL_PREFIX } from '../constants';
 
 export class Profile extends React.Component {
-  render () {
+  render() {
+    const {
+      teamCity,
+      teamName,
+      playerName,
+      height,
+      weight,
+      pts, reb, ast, pie,
+    } = this.props.playerInfo;
     return (
       <div className="profile">
-
+        <div className="profile-entry player-name">{`${playerName}`}</div>
+        <img
+          className="profile-pic"
+          src={`${PROFILE_PIC_URL_PREFIX}/${this.props.playerInfo.playerId}.png`}
+          alt={this.props.playerInfo.playerName}
+        />
+        <div className="profile-entry">
+          <div className="profile-entry-left">Team</div>
+          <div className="profile-entry-right">{`${teamCity} ${teamName}`}</div>
+        </div>
+        <img
+          className="team-logo"
+          src={`${TEAM_PIC_URL_PREFIX}/${this.props.playerInfo.teamAbbreviation}_logo.svg`}
+          alt={this.props.playerInfo.teamName}
+        />
+        <div className="profile-entry">
+          <div className="profile-entry-left">Height</div>
+          <div className="profile-entry-right">{`${height}`}</div>
+        </div>
+        <div className="profile-entry">
+          <div className="profile-entry-left">Weight</div>
+          <div className="profile-entry-right">{`${weight}`}</div>
+        </div>
+        <div className="profile-entry">
+          <div className="profile-entry-left">PTS</div>
+          <div className="profile-entry-right">{`${pts}`}</div>
+        </div>
+        <div className="profile-entry">
+          <div className="profile-entry-left">REB</div>
+          <div className="profile-entry-right">{`${reb}`}</div>
+        </div>
+        <div className="profile-entry">
+          <div className="profile-entry-left">AST</div>
+          <div className="profile-entry-right">{`${ast}`}</div>
+        </div>
+        <div className="profile-entry">
+          <div className="profile-entry-left">PIE</div>
+          <div className="profile-entry-right">{`${pie}`}</div>
+        </div>
       </div>
     );
   }
