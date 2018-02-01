47aafc1b197bc64fbcf498fb7a06c1724eaabf19
szcbossman <songzicheng3@gmail.com>
2018-01-31 21:02:40 -0800

    tooltip toggle added

--- src/components/DataViewContainer.js
+++ src/components/DataViewContainer.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import { ShotChart } from './ShotChart'
 import {CountSlider} from './CountSlider'
-import { Radio, Row, Col } from 'antd';
+import { Radio, Row, Col, Switch } from 'antd';
 const RadioGroup = Radio.Group;
 
 export class DataViewContainer extends React.Component {
   state = {
     minCount: 2,
-    chartType: 'hexbin',
+    chartType: "hexbin",
+    displayTooltip: true,
   }
   onCountSliderChange = (count) => {
     this.setState({ minCount: count });
@@ -15,22 +16,32 @@ export class DataViewContainer extends React.Component {
   onChartTypeChange = (e) => {
     this.setState({ chartType: e.target.value });
   }
+  onTooltipChange = (displayTooltip) => {
+    this.setState({ displayTooltip });
+  }
 
   render () {
     return (
       <div className="data-view">
-        <ShotChart playerId={this.props.playerId} minCount={this.state.minCount} chartType={this.state.chartType}/>
+        <ShotChart playerId={this.props.playerId}
+                   minCount={this.state.minCount}
+                   chartType={this.state.chartType}
+                   displayTooltip={this.state.displayTooltip}
+        />
         <div className="filters">
           {this.state.chartType == "hexbin" ? <CountSlider onCountSliderChange={this.onCountSliderChange}/> : null}
           <Row>
-            <Col span={12} offset={3}>
+            <Col span={12} offset={2}>
               <RadioGroup onChange={this.onChartTypeChange} value={this.state.chartType}>
                 <Radio value="hexbin">Hexbin</Radio>
                 <Radio value="scatter">Scatter</Radio>
               </RadioGroup>
             </Col>
             <Col span={6}>
-
+              <Switch checkedChildren="On" unCheckedChildren="Off"
+                      defaultChecked
+                      onChange={this.onTooltipChange}
+              />
             </Col>
           </Row>
         </div>
