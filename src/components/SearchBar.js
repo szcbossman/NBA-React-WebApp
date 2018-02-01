3ec7e9f5144b3b13366af59515229980d09fdf03
szcbossman <songzicheng3@gmail.com>
2018-01-31 22:14:17 -0800

    searchBar image and string fully completed

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import nba from 'nba'
 import { AutoComplete, Icon, Input } from 'antd';
+import { PROFILE_PIC_URL_PREFIX } from '../constants'
+const Option = AutoComplete.Option;
 
 function onSelect(value) {
   console.log('onSelect', value);
@@ -13,7 +15,10 @@ export class SearchBar extends React.Component {
 
   handleSearch = (value) => {
     this.setState({
-      dataSource: !value ? [] : nba.searchPlayers(value).map(player => player.fullName)
+      dataSource: !value ? [] : nba.searchPlayers(value).map(player => ({
+        fullName: player.fullName,
+        playerId: player.playerId,
+      }))
     });
   }
 
@@ -24,15 +29,23 @@ export class SearchBar extends React.Component {
 
   render() {
     const { dataSource } = this.state;
+    const options = dataSource.map((player) => (
+      <Option key={player.fullName} value={player.fullName} className="player-option">
+        <img className="player-option-image" src={`${PROFILE_PIC_URL_PREFIX}/${player.playerId}.png`}/>
+        <span className="player-option-label">{player.fullName}</span>
+      </Option>
+    ));
     return (
       <AutoComplete
         className="search-bar"
-        dataSource={dataSource}
+        dataSource={options}
         onSelect={this.onSelect}
         onSearch={this.handleSearch}
         placeholder="Search your favorite NBA player!"
         size="large"
+        optionLabelProp="value"
       >
+
         <Input suffix={<Icon type="search" className="certain-category-icon" />} />
       </AutoComplete>
     );
