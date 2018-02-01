cfc1c39870e7bb833e058162398b38229aa65757
szcbossman <songzicheng3@gmail.com>
2018-01-31 22:20:38 -0800

    countSlider data type handle

--- src/components/CountSlider.js
+++ src/components/CountSlider.js
@@ -6,6 +6,7 @@ export class CountSlider extends React.Component {
     inputValue: 2,
   }
   onChange = (value) => {
+    value = Number(value) || 2;
     this.setState({
       inputValue: value,
     });
