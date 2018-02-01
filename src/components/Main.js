53f641dd8a4db62f2495afec985cbf608454af56
szcbossman <songzicheng3@gmail.com>
2018-01-31 21:21:09 -0800

    searchBar initially added

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,6 +2,7 @@ import React from 'react'
 import nba from 'nba'
 import { Profile } from './Profile'
 import { DataViewContainer } from './DataViewContainer'
+import { SearchBar } from './SearchBar'
 
 export class Main extends React.Component {
   state = {
@@ -20,8 +21,11 @@ export class Main extends React.Component {
   render () {
     return (
       <div className="main">
-        <Profile playerInfo={this.state.playerInfo}/>
-        <DataViewContainer playerId={this.state.playerInfo.playerId}/>
+        <SearchBar/>
+        <div className="player">
+          <Profile playerInfo={this.state.playerInfo}/>
+          <DataViewContainer playerId={this.state.playerInfo.playerId}/>
+        </div>
       </div>
     );
   }
